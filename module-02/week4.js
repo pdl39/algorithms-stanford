7cd1c980232ac5b23b5891539993f756e5c02647
pdl39 <dhlee9111@gmail.com>
2021-11-10 19:14:23 -0500

    module-02: week4 brute force

---
+++ module-02/week4.js
@@ -0,0 +1,52 @@
+const twoSumVariant = (nums) => {
+  const sorted = nums.sort((a, b) => a - b);
+  console.log(sorted);
+
+  const seenTargets = new Set();
+
+  for (let target = -10000; target < 10001; target++) {
+    console.log({target});
+    for (let i = 0; i < sorted.length; i++) {
+      const y = target - sorted[i];
+
+      if (y > sorted[sorted.length - 1]) continue;
+      if (y < sorted[0]) continue;
+
+      if (binarySearch(sorted, y) >= 0) {
+        seenTargets.add(target);
+      }
+    }
+  }
+
+  return seenTargets.size;
+}
+
+const binarySearch = (arr, key) => {
+  let low = 0;
+  let high = arr.length - 1;
+
+  if (key > arr[high] || key < arr[low]) return -1;
+
+  while (low <= high) {
+    const mid = low + Math.floor((high - low) / 2);
+
+    if (arr[mid] === key) return mid;
+
+    if (arr[mid] < key) {
+      low = mid + 1;
+    }
+    else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+
+const parseData = require('../parseData');
+const nums = parseData('./week4.txt', '\n', 'num');
+
+// console.log(nums);
+
+console.log(twoSumVariant(nums))
