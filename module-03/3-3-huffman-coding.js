afa68a0edcd2bc6c29140dcb0d3d7c5d4859a013
pdl39 <dhlee9111@gmail.com>
2021-11-15 07:14:48 -0500

    Update - huffman coding algorithm correctly returns output

--- module-03/3-3-huffman-coding.js
+++ module-03/3-3-huffman-coding.js
@@ -14,6 +14,9 @@ const huffmanCode = (weights) => {
     const a = prefixMinHeap.poll(); // 1st min
     const b = prefixMinHeap.poll(); // 2nd min
 
+    a.bit = '0';
+    b.bit = '1';
+
     // Create a new internal node with weight = the sum of the weights of a & b.
     const newNode = new Node(`${a.symbol},${b.symbol}`, a.weight + b.weight);
     // Assign a as its left child, b as its right child.
@@ -25,20 +28,47 @@ const huffmanCode = (weights) => {
   }
 
   const huffmanTree = prefixMinHeap.poll();
-  const maxCodeLength = findMaxDepth(huffmanTree);
-  const minCodeLength = findMinDepth(huffmanTree);
 
-  return {maxCodeLength, minCodeLength};
+  const codeLen = {
+    max: -Infinity,
+    min: Infinity
+  }
+
+  getCode(huffmanTree, '', codeLen); // this will print the code for each symbol and get the max & min code lengths for the huffman tree in one pass. If we dont need to print codes, then we can just use the findMaxAndMinDepths function to get max & min code lengths:
+  const [max, min] = findMaxAndMinDepths(huffmanTree);
+  console.log({max, min});
+
+  return {
+    maxCodeLength: codeLen.max,
+    minCodeLength: codeLen.min
+  };
 }
 
-const findMaxDepth = (tree) => {
-  if (!tree) return 0;
-  return Math.max(findMaxDepth(tree.left), findMaxDepth(tree.right)) + 1;
+const getCode = (node, code, codeLen) => {
+  const newCode = code + node.bit;
+
+  if (node.left) {
+    getCode(node.left, newCode, codeLen);
+  }
+  if (node.right) {
+    getCode(node.right, newCode, codeLen);
+  }
+
+  if (!node.left && !node.right) {
+    codeLen.max = Math.max(codeLen.max, newCode.length);
+    codeLen.min = Math.min(codeLen.min, newCode.length);
+    console.log(`Huffman Code for symbol ${node.symbol}:\n --> ${newCode}`);
+  }
 }
 
-const findMinDepth = (tree) => {
-  if (!tree) return 0;
-  return Math.min(findMinDepth(tree.left), findMinDepth(tree.right)) + 1;
+const findMaxAndMinDepths = (node) => {
+  if (!node) return [-1, -1]; // Since depth is 0 for the root, we need to count from -1 for none nodes so that leaf nodes will start depth counting from 0.
+  const [leftMaxDepth, leftMinDepth] = findMaxAndMinDepths(node.left);
+  const [rightMaxDepth, rightMinDepth] = findMaxAndMinDepths(node.right);
+
+  const maxDepth = Math.max(leftMaxDepth, rightMaxDepth) + 1;
+  const minDepth = Math.min(leftMinDepth, rightMinDepth) + 1;
+  return [maxDepth, minDepth];
 }
 
 class Node {
@@ -47,6 +77,7 @@ class Node {
     this.weight = weight;
     this.left = null;
     this.right = null;
+    this.bit = '';
   }
 }
 
