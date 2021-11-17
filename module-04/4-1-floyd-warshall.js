78bcf036e800d0c548f260e832757d9b82fb0171
pdl39 <dhlee9111@gmail.com>
2021-11-17 01:28:33 -0500

    Update - preprocessing: change edges data struxture

--- module-04/4-1-floyd-warshall.js
+++ module-04/4-1-floyd-warshall.js
@@ -1,5 +1,12 @@
 const floydWarshall = (V, vertices, edges) => {
-
+  // First, prepare the base matrix A for only direct edges between each vertex of the graph.
+  //
+  const A = new Array(V + 1).fill(null).map(() => new Array(V + 1).fill(Infinity));
+  for (let i = 1; i < V + 1; i++) {
+    for (let j = 1; j < V + 1; j++) {
+      A[i, j] = edges[i][j]['dist'] ?? Infinity;
+    }
+  }
 }
 
 
@@ -12,13 +19,13 @@ class Edge {
 }
 
 
-// PRE-PROCESSING: ASSIGNMENT INPUTS
+// PRE-PROCESSING: ASSIGNMENT INPUTS (g1, g2, g3)
 // g1
 const parseData = require('../parseData');
 const edgeNcosts = parseData('./4-1-floyd-warshall-g1.txt', '\n');
-const V1 = Number(edgeNcosts[0]);
-const E1 = Number(edgeNcosts[1]);
-const edges1 = [];
+const V1 = Number(edgeNcosts[0].split(' ')[0]);
+const E1 = Number(edgeNcosts[0].split(' ')[1]);
+const edges1 = {};
 let vertices1 = new Set();
 
 
@@ -29,17 +36,22 @@ for (let i = 1; i < edgeNcosts.length; i++) {
   const v = Number(edgeNcosts[i][1]);
   const dist = Number(edgeNcosts[i][2]);
 
-  edges1.push(new Edge(u, v, dist));
+  if (!edges1[u]) {
+    edges1[u] = {};
+  }
+
+  edges1[u][v] = dist;
   vertices1.add(u);
   vertices1.add(v);
 }
+vertices1 = Array.from(vertices1);
 
 
 // g2
-const edgeNcosts2 = parseData('./4-1-floyd-warshall-g1.txt', '\n');
-const V2 = Number(edgeNcosts2[0]);
-const E2 = Number(edgeNcosts2[1]);
-const edges2 = [];
+const edgeNcosts2 = parseData('./4-1-floyd-warshall-g2.txt', '\n');
+const V2 = Number(edgeNcosts2[0].split(' ')[0]);
+const E2 = Number(edgeNcosts2[0].split(' ')[1]);
+const edges2 = {};
 let vertices2 = new Set();
 
 
@@ -50,17 +62,22 @@ for (let i = 1; i < edgeNcosts2.length; i++) {
   const v = Number(edgeNcosts2[i][1]);
   const dist = Number(edgeNcosts2[i][2]);
 
-  edges2.push(new Edge(u, v, dist));
+  if (!edges2[u]) {
+    edges2[u] = {};
+  }
+
+  edges2[u][v] = dist;
   vertices2.add(u);
   vertices2.add(v);
 }
+vertices2 = Array.from(vertices2);
 
 
 // g3
-const edgeNcosts3 = parseData('./4-1-floyd-warshall-g1.txt', '\n');
-const V3 = Number(edgeNcosts3[0]);
-const E3 = Number(edgeNcosts3[1]);
-const edges3 = [];
+const edgeNcosts3 = parseData('./4-1-floyd-warshall-g3.txt', '\n');
+const V3 = Number(edgeNcosts3[0].split(' ')[0]);
+const E3 = Number(edgeNcosts3[0].split(' ')[1]);
+const edges3 = {};
 let vertices3 = new Set();
 
 
@@ -71,15 +88,35 @@ for (let i = 1; i < edgeNcosts3.length; i++) {
   const v = Number(edgeNcosts3[i][1]);
   const dist = Number(edgeNcosts3[i][2]);
 
-  edges3.push(new Edge(u, v, dist));
+  if (!edges3[u]) {
+    edges3[u] = {};
+  }
+
+  edges3[u][v] = dist;
   vertices3.add(u);
   vertices3.add(v);
 }
+vertices3 = Array.from(vertices3);
 
+// edges (adjacency list) data structure:
+/*
+{
+  i: {
+    j1: dist,
+    j2: dist,
+    ...
+  },
+  ...
+}
+*/
 
-console.log({vertices1, edges1});
-console.log({vertices2, edges2});
-console.log({vertices3, edges3});
+// console.log({vertices1, edges1}, V1, E1);
+// console.log({vertices2, edges2}, V2, E2);
+// console.log({vertices3, edges3}, V3, E3);
+
+// console.log(edges1[334]);
+// console.log(edges2[334]);
+// console.log(edges3[334]);
 
 // RUN
 // console.log(floydWarshall(V1, vertices1, edges1));
